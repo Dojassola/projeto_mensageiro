dbae6a97acc1531a24e09272b1c0df3d15145e7b
dojassola <59090344+Dojassola@users.noreply.github.com>
2026-07-15 23:09:13 -0300

    adiciona pagination, em teorica vai melhorar performance do app para sempre

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 24
-        versionName = "0.23.7"
+        versionName = "0.24.0"
 
         ndk {
             abiFilters += setOf("armeabi-v7a", "arm64-v8a")
